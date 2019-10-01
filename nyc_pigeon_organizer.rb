beb43428a469f0d6a37cb4d938209811a229c674
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-10-01 19:16:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attribute, attribute_hash|
+    attribute_hash.each do |categories, names|
+      names.each do |name|
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        if !new_hash[name][attribute]
+          new_hash[name][attribute] = []
+        end
+      new_hash[name][attribute] << categories.to_s
+      end
+    end
+  end
+  new_hash
 end
+
