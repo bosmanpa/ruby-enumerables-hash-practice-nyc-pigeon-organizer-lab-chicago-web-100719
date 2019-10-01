bb0bcf2987e1ec386c26e052d74fd11278f19525
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-10-01 18:25:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,9 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |attribute, attribute_hash|
-    attribute_hash.each do |attribute_key, names|
+    attribute_hash.each do |categories, names|
       names.each do |name|
+        if new_hash[names[name]]
         
   
   
